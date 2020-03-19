bdf282e863a6d258fd73d62f94fca5fbffdec495
ali <ali_raza995@hotmail.com>
2020-03-19 10:45:46 +0300

    added indexing omim titles and updated data processing

---
+++ api/lookup/ingest/omim.py
@@ -0,0 +1,72 @@
+import logging 
+
+from django.conf import settings
+from api.lookup.ingest.source import Source
+from api.rdf.namespace import OMIM
+
+import api.lookup.lookup_elasticsearch as lookup_es  
+import pandas as pd
+
+
+logger = logging.getLogger(__name__)
+
+class OMIMValueset(Source):
+
+    def __init__(self):
+        super().__init__('OMIM', 'Disease')
+        self.valueset = None
+        self.entities = None
+        self.url = 'https://data.omim.org/downloads/' + settings.OMIM_KEY + '/mimTitles.txt'
+        self.df = None
+
+    def fetch(self):
+        logger.info("Started fetching data for valueset %s", self.name)
+        self.df = pd.read_csv(self.url, sep='\t', skiprows=3, skipfooter=13, names=["prefix",	"mim_number", "preferred_title", "alternative_titles", "included_titles"]) 
+        logger.info("Finished fetching data: entities=%d", self.df.size)
+
+    def map(self):
+        self.valueset = {
+            "valueset" : self.name,
+            "name" : "Online Mendelian Inheritance in Man"
+        }
+
+        self.df =  self.df[self.df['prefix'] != "Caret"]
+        self.df['mim_number'] = self.df['mim_number'].astype(str)
+        self.df['mim_number'] = str(OMIM.uri) + self.df['mim_number']
+        self.df['preferred_title'] = self.df['preferred_title'].astype(str)
+        self.df['alternative_titles'] = self.df['alternative_titles'].astype(str)
+        self.df['included_titles'] = self.df['included_titles'].astype(str)
+
+        logger.info('head: %s', self.df.head())
+        self.entities = list(map(lambda row:self.map_entity(row), self.df.itertuples()))
+        logger.info("Finished mapping data: entities=%d", len(self.entities))
+
+    def write(self):
+        logger.info("Started indexing valueset %s", self.valueset)
+        lookup_es.delete_valueset(self.valueset['valueset'])
+        lookup_es.index(lookup_es.VALUESET_INDEX_NAME, self.valueset)
+        for entity in self.entities:
+            lookup_es.index(lookup_es.ENTITY_INDEX_NAME, entity)
+    
+        logger.info("Finished indexing valueset %s", self.valueset)
+
+    def map_entity(self, row):
+        obj = {}
+        obj["entity"] =  getattr(row, 'mim_number')
+        obj["label"] =  getattr(row, 'preferred_title')
+        obj["synonym"] = [] 
+        pref_titles = getattr(row, 'preferred_title')
+        if pref_titles and 'NaN' not in pref_titles:
+            obj["synonym"] = pref_titles.split(';')
+
+        alternative_titles = getattr(row, 'alternative_titles')
+        if alternative_titles and 'NaN' not in alternative_titles:
+            obj["synonym"] = obj["synonym"]  + alternative_titles.split(';')
+
+        included_titles = getattr(row, 'included_titles')
+        if included_titles and 'NaN' not in included_titles:
+            obj["synonym"] = obj["synonym"]  + included_titles.split(';')
+
+        obj["valueset"] =  self.name
+        obj["entity_type"] = self.entity_type
+        return obj
---
+++ api/training/data_processor.py
@@ -0,0 +1,97 @@
+import tarfile
+import os
+import glob
+import shutil
+import subprocess
+import csv
+
+from django.conf import settings
+
+from api.rdf.namespace import OBO
+from rdflib import Graph, RDF
+from api.training.generate_graph import *
+
+from os import listdir
+from os.path import isfile, join, splitext, exists
+from pathlib import Path
+
+
+logger = logging.getLogger(__name__)
+
+ONTOLOGY_DIR = getattr(settings, 'ONTOLOGY_DIR', None)
+KGE_DIR = getattr(settings, 'KGE_DIR', None)
+TRAINING_SET_DIR = getattr(settings, 'TRAINING_SET_DIR', None)
+
+def generate_nt(file):
+    logger.info("Converting rdf file '{file}' to nt format".format(file=file))
+    store = Graph()
+    store.load(file)
+    out_store = Graph()
+    stmts = list(store.subjects(RDF.predicate, OBO.RO_0002200))
+    phenotype_stmts = []
+    for stmt in stmts:
+        sub = store.value(stmt, RDF.subject)
+        predicate = store.value(stmt, RDF.predicate)
+        obj = store.value(stmt, RDF.object)
+        out_store.add([sub, predicate, obj])
+
+    out_store.serialize('{folder}/{filename}.nt'.format(folder=TRAINING_SET_DIR, filename=file), format='nt')
+    out_store.remove((None, None, None))
+    store.remove((None, None, None))
+
+def rdf2nt(ds_path):
+    files = [file for file in os.listdir(ds_path) if isfile(file)]
+    
+    if len(files) < 1:
+        raise Exception("no file in dataset exists to process")
+
+    for entry in files:
+        if isfile(join(entry)):
+            generate_nt(entry)
+        
+
+def process_dataset():
+    file = archive.find_latest_file()
+    file_path = join(settings.TARGET_DATA_DIR, file.full_name)
+    if exists(file_path):
+        logger.info("Extracting data archive...")
+        ds_tar = tarfile.open(file_path)
+        ds_tar.extractall(KGE_DIR)
+        ds_tar.close()
+    
+    ds_dir = join(KGE_DIR, 'data-*')
+    ds_path = glob.glob(ds_dir)[0]
+    rdf2nt(ds_path)
+    shutil.rmtree(ds_path)
+    logger.info("Completed dataset preprocessing")
+
+
+def process_ontology():
+    os.chdir('load')
+    files = [file for file in os.listdir(ONTOLOGY_DIR)]
+    for entry in files:
+        logger.info("Processing ontology '%s'", entry)
+        args = join(ONTOLOGY_DIR, entry) + ", elk," + TRAINING_SET_DIR
+        logger.info("Running process ontology with arguments '%s'", args)
+        process = subprocess.Popen("gradle processontology '-PcliArgs=" + args + "'", stdout=subprocess.PIPE, shell=True)
+        for line in process.stdout:
+            logger.info(line.strip())
+    
+    logger.info("Extracted axioms from ontologies")
+    files = [file for file in os.listdir(TRAINING_SET_DIR) if isfile(join(TRAINING_SET_DIR, file)) and 'ls' in splitext(file)[1]]
+    for entry in files:
+        logger.info("Converting axioms for file '%s' to a graph", entry)
+
+        with open(join(TRAINING_SET_DIR, entry), "r") as f:
+            outFile = join(TRAINING_SET_DIR, entry + ".tsv")
+            with open(outFile, 'w') as file:
+                writer = csv.writer(file, delimiter='\t')
+
+                for line in f.readlines():
+                    result = convert_graph(line.strip())
+                    if not result:
+                        continue
+                    
+                    writer.writerows(result)
+            
+        logger.info("Finished converting axioms for file '%s' to a graph", entry)
